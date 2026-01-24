e44efe1621644b95e5287901f2faad2048fc43ef
Admin <josejordan20222@gmail.com>
2026-01-24 23:07:52 +0000

    Fix White Screen: Replace 53MB GIF with CSS Background

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 import GlassLogin from "@/components/GlassLogin";
+import Background from "@/components/crystalline/Background";
 
 export default function Home() {
     return (
         <main className="relative flex min-h-screen w-full flex-col items-center justify-center overflow-hidden bg-black">
 
-            {/* 1. EL BACKGROUND (GIF) */}
-            <div className="absolute inset-0 z-0">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                    src="/background.gif"
-                    alt="Background Animation"
-                    className="h-full w-full object-cover"
-                />
-            </div>
+            {/* 1. EL BACKGROUND (CSS Optimized) */}
+            <Background />
 
             {/* 2. EL CONTENIDO (Z-Index bajo para no tapar el modal de World ID) */}
             <div className="relative z-10 px-4">
