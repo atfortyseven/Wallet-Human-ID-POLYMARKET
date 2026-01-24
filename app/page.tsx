bff4129da8c6234edfd259043bc51fff785c2485
Admin <josejordan20222@gmail.com>
2026-01-24 21:00:07 +0000

    style: fix full width layout constraints

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import GlassLogin from "@/components/GlassLogin";
 
 export default function Home() {
     return (
-        <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-black">
+        <main className="relative flex min-h-screen w-full flex-col items-center justify-center overflow-hidden bg-black">
 
             {/* 1. EL BACKGROUND (GIF) */}
             <div className="absolute inset-0 z-0">
